cdf5e4777c701a544ce4c3cd7899614cda1ffc7f
akshya-zen <135795863+akshya-zen@users.noreply.github.com>
2023-06-24 13:34:07 +0530

    reactSamplesJune23rd

---
+++ welcome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const welcome = () => {
+    return(
+        <div>
+        <img
+            src = "https://i.imgur.com/QIrZWGIs.jpg"
+            alt = "Lost img"
+        />
+        <h1>Sample text from welcome.js</h1>
+        </div>
+
+    );
+
+}
+export default welcome;
